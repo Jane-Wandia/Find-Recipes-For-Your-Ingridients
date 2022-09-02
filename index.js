6895568cf087aa2d1a50ac014ae5e408df80770a
janekigenyi <janekigenyi@gmail.com>
2022-09-03 02:59:48 +0300

    updated javascript code

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const searchBtn=document.getElementById('search-btn');
-const mealList=document.getElementById('meal');
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 //adding event listner
-searchBtn.addEventListener('click',getMealList);
+searchBtn.addEventListener('click', getMealList);
 
 //getting meal list matching the ingridients
 function getMealList(){
-    let searchInputTxt=document.getElementById
+    let searchInputTxt = document.getElementById
     ('search-input').value.trim();
-    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=egg')
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-    })
+        let html ="";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}>
+                        <div class="meal-image">
+                            <img src = "${meal.strMealThumb}"
+                            alt = "food"
+                        </div>
+                            <div class="meal-name">
+                                <h3>${meal.strMeal}</h3>
+                                <a href="#" class="recipe-btn">Get Recipe</a>
+                            </div>
+                    </div>
+                `;
+            });
+        }
+        mealList.innerHTML =html
+    });
 }
